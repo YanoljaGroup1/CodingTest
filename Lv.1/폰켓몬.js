124e858a93436e8dbe4696d5ea76bb00411ac0b8
furaha707 <zxcvyein950823@gmail.com>
2023-09-02 10:54:26 +0900

    2023.09.02

--- Lv.1/폰켓몬.js
+++ Lv.1/폰켓몬.js
@@ -1,16 +1,17 @@
 function solution(nums) {
   var answer = 0;
   
-const uniqueNumbers = new Set(nums);
-const uniqueCount = uniqueNumbers.size;
-const halfLength = nums.length / 2;
+  //  배열에서 중복되는 숫자 제거
+  const uniqueNumbers = new Set(nums);
+  const uniqueCount = uniqueNumbers.size;
 
-if (uniqueCount <= halfLength) {
-  return uniqueCount;
-} else {
-  return halfLength;
-}
-  
+  const halfLength = nums.length / 2;
+
+  if (uniqueCount <= halfLength) {
+    return uniqueCount;
+  } else {
+    return halfLength;
+  }
 }
 
 solution([3,3,3,2,2,4])

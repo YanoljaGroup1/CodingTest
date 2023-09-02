2c9f4733239812b76b24d5ba619481da420e6f7b
furaha707 <zxcvyein950823@gmail.com>
2023-09-02 15:08:36 +0900

    2023.09.02

---
+++ Lv.1/소수찾기.js
@@ -0,0 +1,19 @@
+function solution(n) {
+  let Count = 0;
+
+  for (let num = 2; num <= n; num++) {
+    let isTrue = true;
+    for (let i = 2; i*i <= num; i++) {
+      if (num % i === 0) {
+        isTrue = false;
+        break;
+      }
+    }
+    if (isTrue) {
+      Count++;
+    }
+  }
+  return Count;
+}
+
+solution(10)

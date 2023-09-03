f3c115146e40289d2a0844053126a833d882b1e8
Yamyam-code <rnffjt@gmail.com>
2023-09-03 20:45:14 +0900

    과일장수

---
+++ 2023-08-31/과일장수.js
@@ -0,0 +1,21 @@
+function solution(k, m, score) {
+  let answer = 0;
+  let boxs = score.length / m;
+  let box = [];
+  let apples = score.sort((a, b) => b - a);
+  apples.forEach((item, index) => {
+    if (box.length <= boxs) {
+      if (index % m === 0) {
+        box.push([]);
+      }
+      box[box.length - 1].push(item);
+    }
+  });
+  box = box.filter((e) => e.length >= m);
+  box.forEach((e) => {
+    e.sort((a, b) => a - b);
+    answer += e[0] * e.length;
+  });
+  return answer;
+}
+console.log(solution(3, 4, [1, 2, 3, 1, 2, 3, 1]));

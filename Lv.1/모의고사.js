c14bab0d50186d01184cfaa44c7f3645e8b1a5ba
furaha707 <zxcvyein950823@gmail.com>
2023-09-04 10:56:51 +0900

    2023.09.04

--- Lv.1/모의고사.js
+++ Lv.1/모의고사.js
@@ -1,22 +1,32 @@
 function solution(answers) {
-    var answer = [];
-    var a1 = [1, 1, 3, 3, 2];
-    var a2 = [2, 1, 2, 4, 5, 1, 2, 3];
-    var a3 = [3, 2, 3, 1, 2, 1, 2, 3, 4, 5];
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
 
-    var a1c = answers.filter((a,i)=> a === a1[i%a1.length]);
-    var a2c = answers.filter((a,i)=> a === a2[i%a2.length]);
-    var a3c = answers.filter((a,i)=> a === a3[i%a3.length]);
-    console.log(a1c, a2c, a3c)
-    // answer의 갯수에 따른 인덱스 까지만 비교가 가능하므로 a1, a2, a3 의 길이가 그 보다 더 컸을 때 정확한 비교가 어려움
-    var max = Math.max(a1c,a2c,a3c);
+    const scores = [0, 0, 0]; 
 
-    if (a1c === max) {answer.push(1)};
-    if (a2c === max) {answer.push(2)};
-    if (a3c === max) {answer.push(3)};
+    for (let i = 0; i < answers.length; i++) {
+        for (let j = 0; j < 3; j++) {
+            const pattern = patterns[j];
+            if (answers[i] === pattern[i % pattern.length]) {
+                scores[j]++;
+            }
+        }
+    }
 
-    console.log(answer);
-    return answer;
+    const maxScore = Math.max(...scores);
+    const result = [];
+
+    for (let i = 0; i < scores.length; i++) {
+        if (scores[i] === maxScore) {
+            result.push(i + 1);
+        }
+    }
+
+    return result;
 }
 
-solution([1,2,3,4,5])
+const answers = [1, 2, 3, 4, 5];
+const result = solution(answers);

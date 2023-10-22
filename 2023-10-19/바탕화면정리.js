181e625377a0e353730c998c93e21d33b3ff43a6
Yamyam-code <rnffjt@gmail.com>
2023-10-22 22:01:12 +0900

    바탕화면정리

---
+++ 2023-10-19/바탕화면정리.js
@@ -0,0 +1,37 @@
+function solution(wallpaper) {
+  let answer = [];
+  let lowY = [50];
+  let lowX = [50];
+  let highX = [0];
+  let highY = [0];
+  for (let i = 0; i < wallpaper.length; i++) {
+    for (let j = 0; j < wallpaper[i].length; j++) {
+      if (wallpaper[i][j] === '#') {
+        if (i <= lowY) {
+          lowY = [i];
+        }
+        if (j <= lowX) {
+          lowX = [j];
+        }
+        if (i >= highY) {
+          highY = [i + 1];
+        }
+        if (j >= highX) {
+          highX = [j + 1];
+        }
+      }
+    }
+  }
+  answer = [...lowY, ...lowX, ...highY, ...highX];
+  return answer;
+}
+
+console.log(
+  solution([
+    '..........',
+    '.....#....',
+    '......##..',
+    '...##.....',
+    '....#.....',
+  ])
+);
